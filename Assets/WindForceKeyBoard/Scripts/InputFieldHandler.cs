a43b7a60d0016fa16fad53f652ea6899836a6428
qais <totah_60@hotmail.com>
2018-01-16 15:29:50 -0800

    First Commit

---
+++ Assets/WindForceKeyBoard/Scripts/InputFieldHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputFieldHandler : MonoBehaviour {
+	public KeyBoard[] Inputs;
+	private int count;
+	private int currentField;
+	// Use this for initialization
+	void Start () {
+		count = 0;
+		Inputs = Component.FindObjectsOfType<KeyBoard> ();
+		Ini_Inputs ();
+	}
+
+	private void Ini_Inputs(){
+		if (Inputs != null) {
+			for (int i = 0; i < Inputs.Length; i++) {
+
+				Inputs [i].set_id (count);
+				count++;
+			}
+
+		}
+
+	}
+
+	public void set_currentField(int id){
+
+		currentField = id;
+	}
+
+	public int get_currentField(){
+
+		return currentField;
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/WindForceKeyBoard/Scripts/KeyBoard.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyBoard : MonoBehaviour {
+	AndroidJavaObject activity;
+	AndroidJavaObject context;
+	 private string temptext;
+	[SerializeField]Text text;
+	[SerializeField]Text placeholder;
+	public int id;
+	private InputFieldHandler handler;
+	public int get_id(){
+		return id;
+
+	}
+
+	public void set_id(int c){
+		id = c;
+	}
+
+	// Pass execution context over to the Java UI thread.
+	void Start()
+	{
+		handler = Component.FindObjectOfType<InputFieldHandler> ();
+	}
+	/*
+	void runOnUiThread()
+	{
+		Debug.Log("I'm running on the Java UI thread!");
+		var plugin = new AndroidJavaClass ("openkeyboard.windforceworld.com.keyboardplugin.PluginClass");
+		Debug.Log( plugin.CallStatic<string>("OpenKeyBoard",context));
+	}
+*/
+
+	// Use this for initialization
+	public void OpenKeyBoard () {
+		handler.set_currentField (id);
+		AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+		activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+		context = activity.Call<AndroidJavaObject>("getApplicationContext");
+
+		var plugin = new AndroidJavaClass ("openkeyboard.windforceworld.com.keyboardplugin.PluginClass");
+		Debug.Log(	plugin.CallStatic<string>("OpenKeyBoard",context));
+	}
+	
+
+
+	void Update() {
+		if (id == handler.get_currentField()) {
+			if (text.text.Length <= 0 && !placeholder.IsActive ()) {
+				placeholder.gameObject.SetActive (true);
+				text.gameObject.SetActive (false);
+
+			} else if (text.text.Length > 0 && !text.IsActive ()) {
+				placeholder.gameObject.SetActive (false);
+
+				text.gameObject.SetActive (true);
+
+
+			}
+			foreach (char c in Input.inputString) {
+				if (c == '\b') { // has backspace/delete been pressed?
+					if (text.text.Length != 0) {
+						text.text = text.text.Substring (0, text.text.Length - 1);
+					}
+				} else if ((c == '\n') || (c == '\r')) { // enter/return
+					print ("User entered their name: " + text.text);
+				} else {
+					text.text += c;
+				}
+				Debug.Log (text);
+			}
+		}
+	
+	}
+
+}
